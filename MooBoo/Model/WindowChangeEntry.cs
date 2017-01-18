83b0052274989b62683411fe931b61621efd9237
iggytoto <iggytoto@gmail.com>
2017-01-19 00:44:35 +0300

    Model updated

---
+++ MooBoo/Model/WindowChangeEntry.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MooBoo.Model
+{
+    public class WindowChangeEntry
+    {
+
+        #region Constructor
+        public WindowChangeEntry(string fileName, string category, DateTime switchToTime)
+        {
+            FileName = fileName;
+            Category = category;
+            SwitchToTime = switchToTime;
+        }
+
+        #endregion
+
+        #region Properties
+
+        public string FileName { get; private set; }
+        public string Category { get; private set; }
+        public DateTime SwitchToTime { get; private set; }
+
+        #endregion
+    }
+}
