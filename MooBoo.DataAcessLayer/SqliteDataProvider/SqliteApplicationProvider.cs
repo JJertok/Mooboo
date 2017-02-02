87952eaf49c749740aae269807e9fd82878e8cd6
iggytoto <iggytoto@gmail.com>
2017-02-02 22:55:07 +0300

    Application to model and provider

---
+++ MooBoo.DataAcessLayer/SqliteDataProvider/SqliteApplicationProvider.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Mooboo.Utilities;
+using MooBoo.Infrastructure.Interfaces.DataLayer;
+using MooBoo.Model.DataLayer;
+
+namespace MooBoo.DataAcessLayer.SqliteDataProvider
+{
+    class SqliteApplicationProvider : Singleton<SqliteApplicationProvider>, IRepository<Application>
+    {
+        public Application Create(Application obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Application Read(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Application obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(int id)
+        {
+            return SqliteDataBaseProvider.DeleteByIdFromTable(Application.Token, id);
+        }
+    }
+}
---
+++ MooBoo.Model/DataLayer/Application.cs
@@ -0,0 +1,24 @@
+﻿using MooBoo.Infrastructure.Interfaces.DataLayer;
+
+namespace MooBoo.Model.DataLayer
+{
+    public class Application : DataObject
+    {
+        public static string Token = "APPLICATION";
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+        public string FileName { get; set; }
+
+        public override DataObject Clone()
+        {
+            return new Application
+            {
+                Id = Id,
+                Category = null,
+                CategoryId = CategoryId,
+                FileName = FileName
+            };
+        }
+    }
+}
